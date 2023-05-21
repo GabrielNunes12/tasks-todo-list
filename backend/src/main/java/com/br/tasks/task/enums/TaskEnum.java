22a727736c8c950a869670b0016bb47566ab4757
Gabriel <prismaticnetwork12@gmail.com>
2023-05-21 00:51:09 -0300

    feat: implementing deleteById

---
+++ backend/src/main/java/com/br/tasks/task/enums/TaskEnum.java
@@ -0,0 +1,2 @@
+package com.br.tasks.task.enums;public enum TaskEnum {
+}
--- frontend/src/components/TasksList.jsx
+++ frontend/src/components/TasksList.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 export default function TasksList() {
   const [task, setTask] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [message, setMessage] = useState("");
+  const [order, setOrder] = useState("");
   const navigate = useNavigate();
   useEffect(() => {
     axios
@@ -11,25 +14,61 @@ export default function TasksList() {
       .catch((error) => setTask(error));
   }, []);
   const handleEdit = (edit, id, task) => {
-    console.log(edit);
     if (edit) {
       navigate("/tasks", { state: { isEdit: true, id: id, data: task } });
     }
   };
+  const handleDelete = (id) => {
+    setModal(true);
+    axios
+      .delete(`http://localhost:8090/api/v1/tasks/${id}`)
+      .then((response) => setMessage(response.data))
+      .catch((error) => setMessage(error));
+  };
+  const handleCloseModal = () => {
+    setModal(false);
+    window.location.reload();
+  };
+  const orderByAsc = () => {
+    setOrder("asc");
+  };
+  const orderByDesc = () => {
+    setOrder("desc");
+  };
+  useEffect(() => {
+    if (order === "asc") {
+      setTask([...task].sort((a, b) => a.title.localeCompare(b.title)));
+    } else if (order === "desc") {
+      setTask([...task].sort((a, b) => b.title.localeCompare(a.title)));
+    }
+  }, [order]);
   return (
     <div>
+      <button onClick={() => orderByAsc()}>Order by ASC</button>
+      <button onClick={() => orderByDesc()}>Order by DESC</button>
       {task.length > 0
         ? task.map((task) => (
             <div key={task.id}>
               <h1>{task.title}</h1>
               <p>{task.description}</p>
-              <p>{task.isFinished ? "Finished" : "Not finished"}</p>
+              <p>{task.taskEnum === "EM_PROGRESSO" ? "IN PROGRESS" : "DONE"}</p>
               <button onClick={() => handleEdit(true, task.id, task)}>
                 Edit
               </button>
+              <button onClick={() => handleDelete(task.id)}>Delete</button>
             </div>
           ))
         : "No tasks"}
+      {modal ? (
+        <>
+          <button onClick={() => handleCloseModal()}> Close </button>
+          <div>
+            <h1>{message}</h1>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
